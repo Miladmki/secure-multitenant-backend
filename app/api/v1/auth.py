3119f934d7d9123e69ec5192054541dc0c011845
Miladmki <mikaelimilad8@gmail.com>
2025-12-16 04:38:46 +0330

    implement user registration and login functionality with password hashing

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from app.core.security import hash_password
+from app.models.user import User
+from app.core.security import verify_password
 
 from app.db.session import get_db
 from app.schemas.user import UserCreate, UserLogin
@@ -7,17 +10,27 @@ from app.services.user_service import create_user, authenticate_user
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
-@router.post("/register", status_code=status.HTTP_201_CREATED)
+@router.post("/register")
 def register(user_in: UserCreate, db: Session = Depends(get_db)):
-    user = create_user(db, user_in.email, user_in.password)
+    user = User(
+        email=user_in.email,
+        hashed_password=hash_password(user_in.password)
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
     return {"id": user.id, "email": user.email}
-
 @router.post("/login")
 def login(user_in: UserLogin, db: Session = Depends(get_db)):
-    user = authenticate_user(db, user_in.email, user_in.password)
-    if not user:
+    user = db.query(User).filter(User.email == user_in.email).first()
+
+    if not user or not verify_password(
+        user_in.password,
+        user.hashed_password
+    ):
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid credentials"
+            status_code=400,
+            detail="Invalid email or password"
         )
-    return {"message": "login successful"}
+
+    return {"message": "Login successful"}
